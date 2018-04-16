57d989714444a6313b6c5c7e2ca0360cab7884a6
brenbg <bren.bglz7@gmail.com>
2018-04-16 01:07:25 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CheckersBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //#region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+
+        private void InitializeComponent()
+        {
+            this.tablero = new int[8, 8];
+            for (int m = 0; m < 8; m++)
+                for (int n = 0; n < 8; n++)
+                {
+                    if (m % 2 == 0)  //fila par empieza con claro
+                    {
+                        if (n % 2 == 1) //en fila par las columnas impares tiene fichas
+                        {
+                            if (m < 3)
+                                tablero[n, m] = -1;
+                            else if (m > 4)
+                                tablero[n, m] = 1;
+                        }
+                        else
+                        {
+                            tablero[n,m] = 3;
+                        }
+                    }
+                    else //fila impar
+                    {
+                        if (n % 2 == 0) //en fila impar las columnas pares tienen fichas
+                        {
+                            if (m < 3)
+                                tablero[n, m] = -1;
+                            else if (m > 4)
+                                tablero[n, m] = 1;
+                        }
+                        else
+                        {
+                            tablero[n,m]= 3;
+                        }
+                    }
+                }
+
+            //tablero[0,0] = 0;
+            //tablero[0, 1] = -2;
+
+            System.Console.WriteLine("Tablero inicial");
+            for (int m = 0; m < 8; m++)
+            {
+                for (int n = 0; n < 8; n++)
+                {
+                    System.Console.Write(tablero[n, m]);
+                }
+                System.Console.WriteLine(" ");
+            }
+        
+                    
+
+            this.picBoard = new System.Windows.Forms.PictureBox();
+            this.picTile = new System.Windows.Forms.PictureBox();
+            this.lblDetails = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoard
+            // 
+            this.picBoard.Location = new System.Drawing.Point(16, 26);
+            this.picBoard.Margin = new System.Windows.Forms.Padding(4);
+            this.picBoard.Name = "picBoard";
+            this.picBoard.Size = new System.Drawing.Size(560, 560);
+            this.picBoard.TabIndex = 0;
+            this.picBoard.TabStop = false;
+            // 
+            // picTile
+            // 
+            this.picTile.BackColor = System.Drawing.Color.Blue;
+            this.picTile.Location = new System.Drawing.Point(191, 82);
+            this.picTile.Margin = new System.Windows.Forms.Padding(4);
+            this.picTile.Name = "picTile";
+            this.picTile.Size = new System.Drawing.Size(68, 63);
+            this.picTile.TabIndex = 1;
+            this.picTile.TabStop = false;
+            this.picTile.Visible = false;
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDetails.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblDetails.Location = new System.Drawing.Point(0, 608);
+            this.lblDetails.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(725, 28);
+            this.lblDetails.TabIndex = 2;
+            this.lblDetails.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 636);
+            this.Controls.Add(this.lblDetails);
+            this.Controls.Add(this.picTile);
+            this.Controls.Add(this.picBoard);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grid Test";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTile)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        //#endregion
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.PictureBox picBoard;
+        private System.Windows.Forms.PictureBox picTile;
+        private int[,] tablero;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckersBoard
+{
+    public partial class Form1 : Form
+    {
+        List<CellDetails> cells = new List<CellDetails>();
+        const int cCellSize = 70;
+        int boardLeft = 0;
+        int boardTop = 0;
+        int clickCount = 0;
+        CellDetails inicio, fin;
+        CellDetails cNull;
+        int filaInicio, columnaInicio, filaFin, columnaFin;
+        int auxtipo = 0, tipoInicio = 0, tipoFin = 0;
+
+        TextureBrush cOscuro = new TextureBrush(Image.FromFile(@".\Imagenes\cC270.jpg"));
+        TextureBrush cClaro = new TextureBrush(Image.FromFile(@".\Imagenes\cB70.jpg"));
+        TextureBrush cFB = new TextureBrush(Image.FromFile(@".\Imagenes\cOFB70.png"));
+        TextureBrush cFN = new TextureBrush(Image.FromFile(@".\Imagenes\cOFN70.png"));
+        TextureBrush cRB = new TextureBrush(Image.FromFile(@".\Imagenes\cORB70.png"));
+        TextureBrush cRN = new TextureBrush(Image.FromFile(@".\Imagenes\cORN70.png"));
+        
+
+        //int [,] tablero = new int[8,8];
+        public Form1()
+        {
+            InitializeComponent();
+
+            picTile.Size = new Size(cCellSize, cCellSize);
+
+            //picBoard.MouseMove += PicBoard_MouseMove;
+            picBoard.MouseClick += PicBoard_MouseClick;
+            picBoard.Paint += PicBoard_Paint;
+            CreateCells();
+
+            boardLeft = picBoard.Location.X;
+            boardTop = picBoard.Location.Y;
+            cNull = new CellDetails();
+            cNull.tipo = -3; //-3 indica que es null
+            cNull.Dimension = new Rectangle();
+            cNull.Location = new Point(-1,-1);
+            cNull.ubicacion = -1;
+        }
+        private void PicBoard_Paint(object sender, PaintEventArgs e)
+        {
+            if (cells.Count > 0)
+            {
+                
+                
+
+
+                int fila = 0, columna = 0;
+
+                for (int i = 0; i <= 63; i++)
+                {
+                    cells[i].Dimension.Height = 70;
+                    cells[i].Dimension.Width = 70;
+                    cells[i].ubicacion = i;
+                    fila = i / 8;
+                    columna = i % 8;
+
+                    switch (tablero[columna, fila])
+                    {
+                        case -2:
+                            e.Graphics.FillRectangle(cRN, cells[i].Dimension);
+                            cells[i].tipo = -2;
+                            cells[i].ubicacion = i;
+                            break;
+                        case -1:
+                            e.Graphics.FillRectangle(cFN, cells[i].Dimension);
+                            cells[i].tipo = -1;
+                            break;
+                        case 0:
+                            e.Graphics.FillRectangle(cOscuro, cells[i].Dimension);
+                            cells[i].tipo = 0;
+                            break;
+                        case 1:
+                            e.Graphics.FillRectangle(cFB, cells[i].Dimension);
+                            cells[i].tipo = 1;
+                            break;
+                        case 2:
+                            e.Graphics.FillRectangle(cRB, cells[i].Dimension);
+                            cells[i].tipo = 2;
+                            break;
+                        case 3:
+                            e.Graphics.FillRectangle(cClaro, cells[i].Dimension);
+                            cells[i].tipo = 3;
+                            break;
+                    }
+                }
+                /*cOscuro.Dispose();
+                cClaro.Dispose();
+                cFB.Dispose();
+                cFN.Dispose();
+                cRB.Dispose();
+                cRN.Dispose();*/
+            }
+        }
+        private void PicBoard_MouseClick(object sender, MouseEventArgs e)
+        {
+            clickCount++;
+            CellDetails cell = FindCell(e);
+            if (cell != null && cell.Location.X > -1 && cell.Location.Y > -1)
+            {
+                if (((((cell.ubicacion / 8) % 2) != 0) && ((cell.ubicacion % 8) % 2 == 0)) || (((cell.ubicacion / 8) % 2 == 0) && ((cell.ubicacion % 8) % 2 != 0)))
+                {
+                    if (clickCount % 2 != 0) //Primer clic del movimiento (elección de ficha a mover)
+                    {
+                        //picTile.Location = new Point(cell.Dimension.X + boardLeft, cell.Dimension.Y + boardTop);
+                        //picTile.Visible = true;
+                        inicio = cell;
+                        //Console.WriteLine("Inicio " + inicio.Location.X + " " + inicio.Location.Y);
+                    }
+                    else //Segundo clic de movimiento (elección de a dónde se va a mover la ficha)
+                    {
+                        fin = cell;
+                        //Console.Write("Fin " + fin.Location.X + " " + fin.Location.Y);
+
+                        //encontrar posición en tablero
+                        /*filaInicio = inicio.ubicacion / 8;
+                        columnaInicio = inicio.ubicacion % 8;
+                        filaFin = fin.ubicacion / 8;
+                        columnaFin = fin.ubicacion % 28;*/
+                        if (tablero[inicio.ubicacion % 8, inicio.ubicacion / 8] != 0 && tablero[fin.ubicacion % 8, fin.ubicacion / 8] == 0 && Math.Abs(distanciaX(inicio.ubicacion % 8, fin.ubicacion % 8)) == 1 && Math.Abs(distanciaY(inicio.ubicacion / 8, fin.ubicacion / 8)) == 1)
+                        //if (tablero[columnaInicio, filaInicio] != 0 && tablero[columnaFin, filaFin] == 0 /*&& distancia(columnaInicio,filaInicio,columnaFin,filaFin) == 1*/)
+                            {
+                            //intercambia imgs
+                            auxtipo = inicio.tipo;
+                            inicio.tipo = fin.tipo;
+                            fin.tipo = auxtipo;
+                            tablero[inicio.ubicacion % 8, inicio.ubicacion / 8] = inicio.tipo;
+                            tablero[fin.ubicacion % 8, fin.ubicacion / 8] = fin.tipo;
+                            /*tablero[columnaInicio, filaInicio] = inicio.tipo;
+                            tablero[columnaFin, filaFin] = fin.tipo;*/
+                            Console.WriteLine("Tipo Inicio: " + inicio.tipo + "\nTipo fin: " + fin.tipo);
+
+                            this.Refresh();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Movimiento no válido");
+                            MessageBox.Show("Movimiento no válido", "Error", MessageBoxButtons.OK);
+                        }
+
+                        /*Console.WriteLine("Nuevo tablero");
+                        for (int m = 0; m < 8; m++)
+                        {
+                            for (int n = 0; n < 8; n++)
+                            {
+                                Console.Write(tablero[n, m]);
+                            }
+                            Console.WriteLine(" ");
+                        }*/
+                    }
+                }
+
+            }
+            //else Console.WriteLine("Movimiento no válido");
+
+        }
+        /*    private void PicBoard_MouseMove(object sender, MouseEventArgs e)
+            {
+                CellDetails cell = FindCell(e);
+                string details;
+
+                if (cell == null || cell.Location.X < 0 || cell.Location.Y < 0)
+                {
+                    details = "Could not find cell";
+                }
+                else
+                {
+                    details = string.Format("Row: {0}, Col: {1}", cell.Location.Y, cell.Location.X);
+                }
+                lblDetails.Text = String.Format("{0}, {1} = {2}", e.X, e.Y, details);
+            } */
+
+
+        public void imprimeTablero()
+        {
+            Console.WriteLine("Nuevo tablero");
+            for (int m = 0; m < 8; m++)
+            {
+                for (int n = 0; n < 8; n++)
+                {
+                    Console.Write(tablero[n, m]);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+
+        public int distanciaX(int xi, int xf)
+        {
+            return xf - xi;
+        }
+
+        public int distanciaY(int yi, int yf)
+        {
+            return yf - yi;
+        }
+
+        public int ocupado(int xi, int yi)
+        {
+            return tablero[xi, yi];
+        }
+
+        public Boolean desocupado(int xf, int yf)
+        {
+            return tablero[xf, yf] == 0;
+        }
+      
+        private CellDetails FindCell(MouseEventArgs e)
+        {
+            CellDetails ret = null;
+            foreach (CellDetails cell in cells)
+            {
+                if (cell.Dimension.Contains(e.Location))
+                {
+                    ret = cell;
+                    break;
+                }
+            }
+            return ret;
+        }
+        private void CreateCells()
+        {
+            CellDetails cell;
+            Point cellLoc;
+            //int rows = (picBoard.ClientSize.Height / cCellSize);
+            //int cols = (picBoard.ClientSize.Width / cCellSize);
+            int rows = 8, cols = 8, y = 0, x = 0;
+
+            // Loop through the rows
+            for (int row = 0; row < rows; row++)
+            {
+                // Loop through the columns
+                for (int col = 0; col < cols; col++)
+                {
+                    cell = new CellDetails();
+                    cellLoc = new Point();
+                    x = col * cCellSize;
+                    y = row * cCellSize;
+                    //cellLoc.Y = row + 1;
+                    //cellLoc.X = col + 1;
+                    cellLoc.X = x;
+                    cellLoc.Y = y;
+                    cell.Location = cellLoc;
+                    //cell.Dimension = new Rectangle(col * cCellSize, row * cCellSize, cCellSize, cCellSize);
+                    cell.Dimension = new Rectangle(x, y, cCellSize, cCellSize);
+                    cells.Add(cell);
+                }
+            }
+            lblDetails.Text = String.Format("Generated {0} cells.", cells.Count);
+        }
+    }
+
+    public class CellDetails
+    {
+        public CellDetails() { }
+        public void imprimir()
+        {
+            Console.WriteLine( "--Información de celda\nTipo: " + this.tipo + "\nUbicación: " + this.ubicacion + "\nLocation: " + this.Location.ToString() + "\n--fin Información celda");
+        }
+        //public Rectangle Dimension { get; set; }        
+        public Point Location { get; set; }
+        public Rectangle Dimension = new Rectangle(0, 0, 70, 70);
+        public int tipo, ubicacion;
+        
+
+        
+    }
+
+    
+}
